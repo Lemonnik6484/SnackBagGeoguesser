873f78655b07c839ca40b993eff9aed37dea6a1d
Lemonnik6484 <LemonGamer6484@gmail.com>
2024-12-17 22:56:33 +0100

    registration without tokens

--- backend/Code.js
+++ backend/Code.js
@@ -1,3 +1,35 @@
-function doGet(e) {
+const database = JSON.parse(DriveApp.getFileById("1BzXngEDIhn975vLEImSMq8sraCmX1A12").getBlob().getDataAsString());
+
+function return_output(success, message, data = {}) {
+    return ContentService.createTextOutput(JSON.stringify({ success: success, message: message }))
+        .setMimeType(ContentService.MimeType.JSON);
+}
+
+function registerUser(login, password) {
+    if (login && password) {
+        if (database[login]) {
+            return_output(false, "Account already exists");
+        } else {
+            database[login] = {
+                name: login,
+                avatar: "#ffffff",
+                password: password,
+                token: "",
+                tokenExpireDate: ""
+            }
+            return_output(true, "Account created successfully", {token: database[login].token});
+        }
+    } else {
+        return_output(false, "Invalid request");
+    }
+}
+
+function doPost(e) {
     const action = e.parameter.action;
+    const payload = JSON.parse(e.postData.contents || "{}");
+
+    switch (action) {
+        case "register":
+            registerUser(payload["login"], payload["password"]);
+    }
 }
---
+++ globals.d.ts
@@ -0,0 +1,93 @@
+declare var ContentService: {
+    /**
+     * Creates a new TextOutput object for content served as plain text.
+     * @param content Optional string content.
+     * @returns {TextOutput}
+     */
+    createTextOutput: (content?: string) => TextOutput;
+
+    MimeType: {
+        ATOM: 'application/atom+xml',
+        CSV: 'text/csv',
+        HTML: 'text/html',
+        JAVASCRIPT: 'application/javascript',
+        JSON: 'application/json',
+        RSS: 'application/rss+xml',
+        TEXT: 'text/plain',
+        XML: 'application/xml',
+    };
+};
+
+interface TextOutput {
+    append: (text: string) => TextOutput;
+    clear: () => TextOutput;
+    downloadAsFile: (filename: string) => TextOutput;
+    getContent: () => string;
+    getMimeType: () => string;
+    setMimeType: (mimeType: string) => TextOutput;
+    setContent: (content: string) => TextOutput;
+}
+
+declare interface GoogleAppsScriptEvent {
+    parameter: { [key: string]: string };
+    contextPath: string;
+    contentLength: number;
+    queryString: string;
+    postData: {
+        contents: string;
+        length: number;
+        name: string;
+        type: string;
+    };
+}
+
+declare var DriveApp: {
+    /**
+     * Returns the folder with the specified ID.
+     * @param folderId The ID of the folder to retrieve.
+     * @returns {GoogleAppsScript.Drive.Folder} The folder with the given ID.
+     */
+    getFolderById: (folderId: string) => GoogleAppsScript.Drive.Folder;
+
+    /**
+     * Returns the file with the specified ID.
+     * @param fileId The ID of the file to retrieve.
+     * @returns {GoogleAppsScript.Drive.File} The file with the given ID.
+     */
+    getFileById: (fileId: string) => GoogleAppsScript.Drive.File;
+};
+
+declare namespace GoogleAppsScript.Drive {
+    interface Folder {
+        createFile: (name: string, content: string, mimeType: string) => GoogleAppsScript.Drive.File;
+        getId: () => string;
+        getName: () => string;
+        getFiles: () => GoogleAppsScript.Drive.FileIterator;
+    }
+
+    interface File {
+        getId: () => string;
+        getName: () => string;
+        getMimeType: () => string;
+    }
+
+    interface FileIterator {
+        hasNext: () => boolean;
+        next: () => GoogleAppsScript.Drive.File;
+    }
+}
+
+declare namespace GoogleAppsScript.Drive {
+    interface File {
+        getId: () => string;
+        getName: () => string;
+        getMimeType: () => string;
+        getBlob: () => GoogleAppsScript.Base.Blob;
+    }
+}
+
+declare namespace GoogleAppsScript.Base {
+    interface Blob {
+        getDataAsString: () => string;
+    }
+}
